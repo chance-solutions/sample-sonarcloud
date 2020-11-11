1e2b5c8f708ef2e95a5c622a1f3bf6ab07ebd74a
joven <joven@ProBook-440-G2>
2020-11-11 20:50:20 +0800

    initial version

---
+++ common/src/main/java/com/sample/sonarcloud/Common.java
@@ -0,0 +1,9 @@
+package com.sample.sonarcloud;
+
+public class Common {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ service/src/main/java/com/sample/sonarcloud/Service.java
@@ -0,0 +1,9 @@
+package com.sample.sonarcloud;
+
+public class Service {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
